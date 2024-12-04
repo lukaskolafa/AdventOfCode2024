5cf45c4e96e5174f4e613d75b1878f29ceaee9e9
Lukas Kolafa <lukas.kolafa@arvato-scs.com>
2024-12-04 23:31:57 +0100

    day 2 - gold

---
+++ 02 - Red-Nosed Reports/G/Program.cs
@@ -0,0 +1,58 @@
+﻿bool test = false;
+
+string[] allLines = File.ReadAllLines(test ? @"..\..\..\test.txt" : @"..\..\..\input.txt");
+
+int cnt = 0;
+
+for (long i = 0; i < allLines.Length; i++)
+{
+    var line = allLines[i];
+
+    var numbers = line.Split(" ").Select(int.Parse).ToList();
+
+    if (IsSafe(numbers))
+    {
+        cnt++;
+        continue;
+    }
+
+    for (int j = 0; j < numbers.Count; j++)
+    {
+        var newNumbers = new List<int>(numbers);
+        newNumbers.RemoveAt(j);
+
+        if (IsSafe(newNumbers))
+        {
+            cnt++;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(cnt);
+
+bool IsSafe(IList<int> numbers)
+{
+    var diffs = new List<int>();
+    for (int j = 0; j < numbers.Count - 1; j++)
+    {
+        diffs.Add(numbers[j + 1] - numbers[j]);
+    }
+    
+    if (!diffs.All(x => x > 0) && !diffs.All(x => x < 0))
+    {
+        return false;
+    }
+
+    if (diffs.Any(x => x == 0))
+    {
+        return false;
+    }
+    
+    if (diffs.Any(x => x > 3) || diffs.Any(x => x < -3))
+    {
+        return false;
+    }
+
+    return true;
+}
---
+++ 02 - Red-Nosed Reports/S/Program.cs
@@ -0,0 +1,37 @@
+﻿bool test = false;
+
+string[] allLines = File.ReadAllLines(test ? @"..\..\..\test.txt" : @"..\..\..\input.txt");
+
+int cnt = 0;
+
+for (long i = 0; i < allLines.Length; i++)
+{
+    var line = allLines[i];
+
+    var numbers = line.Split(" ").Select(int.Parse).ToArray();
+
+    var diffs = new List<int>();
+    for (int j = 0; j < numbers.Length - 1; j++)
+    {
+        diffs.Add(numbers[j + 1] - numbers[j]);
+    }
+    
+    if (!diffs.All(x => x > 0) && !diffs.All(x => x < 0))
+    {
+        continue;
+    }
+
+    if (diffs.Any(x => x == 0))
+    {
+        continue;
+    }
+    
+    if (diffs.Any(x => x > 3) || diffs.Any(x => x < -3))
+    {
+        continue;
+    }
+
+    cnt++;
+}
+
+Console.WriteLine(cnt);
