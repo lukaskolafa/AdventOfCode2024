1cceb823f3864bdcaec2259c60d0fdef4c64f674
Lukas Kolafa <lukas.kolafa@arvato-scs.com>
2024-12-04 22:07:41 +0100

    day 1 - silver -part I

---
+++ 01 - Historian Hysteria/S/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+bool test = false;
+
+long result = 0;
+
+string[] allLines = File.ReadAllLines(test ? @"..\..\..\test.txt" : @"..\..\..\input.txt");
+
+var left = new List<long>();
+var right = new List<long>();
+
+for (long i = 0; i < allLines.Length; i++)
+{
+    var line = allLines[i];
+    var match = Regex.Match(line, @"^(\d+)\s+(\d+)$");
+    
+    var firstDigit = match.Groups[1].Value;
+    var lastDigit = match.Groups[2].Value;
+
+    left.Add(long.Parse(firstDigit));
+    right.Add(long.Parse(lastDigit));
+}
+
+var leftSorted = left.Order().ToArray();
+var rightSorted = right.Order().ToArray();
+
+for (long i = 0; i < allLines.Length; i++)
+{
+    var diff = rightSorted[i] - leftSorted[i];
+
+    result += diff;
+}
+
+Console.WriteLine(result);
+
+// 626149 too low
