b0ebed1b884202bb32db3f1343fdef306780f293
Lukas Kolafa <lukas.kolafa@arvato-scs.com>
2024-12-04 23:55:48 +0100

    day 3

---
+++ 03 - Mull It Over/G/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+bool test = false;
+
+string text = File.ReadAllText(test ? @"..\..\..\test.txt" : @"..\..\..\input.txt").Replace("\r", "").Replace("\n", "");
+
+Regex cleaner = new Regex(@"don't\(\).*?do\(\)");
+
+text = cleaner.Replace(text, "");
+
+Regex cleaner2 = new Regex(@"don't\(\).*?$");
+
+text = cleaner2.Replace(text, "");
+
+Console.WriteLine(text);
+
+Regex regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+
+var matches = regex.Matches(text);
+
+long result = 0;
+
+foreach (Match match in matches)
+{
+    var n1 = int.Parse(match.Groups[1].Value);
+    var n2 = int.Parse(match.Groups[2].Value);
+    
+    result += n1 * n2;
+}
+
+Console.WriteLine(result);
+
+// 90572004 too high
---
+++ 03 - Mull It Over/S/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+bool test = false;
+
+string text = File.ReadAllText(test ? @"..\..\..\test.txt" : @"..\..\..\input.txt");
+
+Regex regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+
+var matches = regex.Matches(text);
+
+long result = 0;
+
+foreach (Match match in matches)
+{
+    var n1 = int.Parse(match.Groups[1].Value);
+    var n2 = int.Parse(match.Groups[2].Value);
+    
+    result += n1 * n2;
+}
+
+Console.WriteLine(result);
